f1082c213b331ddb51b0af4e48579425c9f09973
Francko <bien.garperio@uap.asia>
2023-11-22 16:37:04 +0800

    Commit for One Flight springboot application

---
+++ Team-2-One-Flight/src/main/java/com/uap/Team2OneFlight/Team2OneFlightApplication.java
@@ -0,0 +1,13 @@
+package com.uap.Team2OneFlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2OneFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2OneFlightApplication.class, args);
+	}
+
+}
---
+++ Team-2-One-Flight/src/test/java/com/uap/Team2OneFlight/Team2OneFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uap.Team2OneFlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2OneFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
